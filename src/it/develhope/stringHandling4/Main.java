270a68451ee75bef7767713f88817319a3c71983
Lorenzo Balestrini <lo.bale@libero.it>
2023-01-03 14:15:15 +0100

    String Handling 4 - correzione

--- src/it/develhope/stringHandling4/Main.java
+++ src/it/develhope/stringHandling4/Main.java
@@ -6,9 +6,9 @@ public class Main {
         String veryLongString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 
-        veryLongString.replaceAll("um", "HUM");
+        String veryLongHUM = veryLongString.replaceAll("um", "HUM");
 
-        String[] veryLongStringArray = veryLongString.split("aliquip");
+        String[] veryLongStringArray = veryLongHUM.split("aliquip");
 
         String sub1 = veryLongStringArray[0];
         String sub2 = veryLongStringArray[1];
